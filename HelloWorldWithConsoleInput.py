72e14e78477d1a387786c4c3dd660bbd5eb7be36
Wayne Burris <waterseeker@Hotmail.com>
2018-08-05 16:26:45 -0500

    the comment shortcut is cmnd + /

--- HelloWorldWithConsoleInput.py
+++ HelloWorldWithConsoleInput.py
@@ -1,21 +1,23 @@
 __author__ = 'Wayne'
-greeting = "Hello"
-name = input("Please enter your name for a demo of escaping characters to create new lines.")
-print(greeting + ' ' + name + ". Prepare to be amazed at my string-splitting abilities!")
+# greeting = "Hello"
+# name = input("Please enter your name for a demo of escaping characters to create new lines.")
+# print(greeting + ' ' + name + ". Prepare to be amazed at my string-splitting abilities!")
+#
+# splitString = "This string has been\nsplit over\nseveral\nlines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t6\t7"
+# print(tabbedString)
+#
+# print('The pet shot owner said "No, no \'e\'s uh,...he\'s resting"')
+# print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
+#
+# anotherSplitString = """This string has been
+# split over
+# several lines"""
+# print(anotherSplitString)
+#
+# print('''The pet shop owner said "No, no, 'e's uh,...he's resting''')
+# print("""The pet shop owner said "No, no, 'e's uh,...he's resting""")
 
-splitString = "This string has been\nsplit over\nseveral\nlines"
-print(splitString)
 
-tabbedString = "1\t2\t3\t4\t5\t6\t7"
-print(tabbedString)
-
-print('The pet shot owner said "No, no \'e\'s uh,...he\'s resting"')
-print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
-
-anotherSplitString = """This string has been
-split over
-several lines"""
-print(anotherSplitString)
-
-print('''The pet shop owner said "No, no, 'e's uh,...he's resting''')
-print("""The pet shop owner said "No, no, 'e's uh,...he's resting""")
