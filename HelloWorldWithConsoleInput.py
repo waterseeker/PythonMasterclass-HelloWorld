ff3e6656d45c13a2c8cfb94df3a27c500a622665
Wayne Burris <waterseeker@Hotmail.com>
2018-08-05 16:18:50 -0500

    triple quotes FTW

--- HelloWorldWithConsoleInput.py
+++ HelloWorldWithConsoleInput.py
@@ -11,3 +11,11 @@ print(tabbedString)
 
 print('The pet shot owner said "No, no \'e\'s uh,...he\'s resting"')
 print("The pet shop owner said \"No, no, 'e's uh,...he's resting\"")
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+print(anotherSplitString)
+
+print('''The pet shop owner said "No, no, 'e's uh,...he's resting''')
+print("""The pet shop owner said "No, no, 'e's uh,...he's resting""")
