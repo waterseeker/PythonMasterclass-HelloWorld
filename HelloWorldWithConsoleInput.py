5e09d825fe3665a6e5e3f6483da947fc660101f5
Wayne Burris <waterseeker@Hotmail.com>
2018-08-05 15:53:31 -0500

    escaping characters

--- HelloWorldWithConsoleInput.py
+++ HelloWorldWithConsoleInput.py
@@ -1,4 +1,7 @@
 __author__ = 'Wayne'
 greeting = "Hello"
-name = input("Please enter your name ")
-print(greeting + ' ' + name)
+name = input("Please enter your name for a demo of escaping characters to create new lines.")
+print(greeting + ' ' + name + ". Prepare to be amazed at my string-splitting abilities!")
+
+splitString = "This string has been\nsplit over\nseveral\nlines"
+print(splitString)
