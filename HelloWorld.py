3935daac03d87748711740eaeeab13d188e68091
Wayne Burris <waterseeker@Hotmail.com>
2018-08-05 15:40:28 -0500

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+print(1 + 2)
+print(7 * 6)
+print()
+print('The End!')
+print("Python's strings are easy to use")
+print('We can even include "quotes" in strings')
+print("hello" + " world")
+greeting = "Hello"
+name = "Wayne"
+print(greeting + name)
+#if we want a space, we can add that too
+print(greeting + ' ' + name)
