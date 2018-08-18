e8c55a79c5e577687be5ebcc7b64ef0474344dc5
Wayne Burris <waterseeker@Hotmail.com>
2018-08-18 11:24:57 -0500

    added example using vstring index to access characters in a string

--- StringExercises.py
+++ StringExercises.py
@@ -1,2 +1,8 @@
 parrot = "Norwegian Blue"
 print(parrot)
+
+# you can print a character at index X in a string by using
+# print(yourStringHere[X])
+# index is 0 based, so the thing in the first position is 0, not 1
+x = 3
+print('The character at index ' + str(x) + ' is ' + parrot[x])
