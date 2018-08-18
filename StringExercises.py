4d22dbec6830678da7fa5cbae37f6c2608447c02
Wayne Burris <waterseeker@Hotmail.com>
2018-08-18 17:30:32 -0500

    test

--- StringExercises.py
+++ StringExercises.py
@@ -59,3 +59,34 @@ print(myString[:3])
 print('and')
 print(myString[0:3])
 print('are the same.')
+print('--------------------------------------------------')
+# if you leave out the second number
+# it'll skip the number of spaces == the first number and then print everything else to the end of the string.
+print(myString[4:])
+print('--------------------------------------------------')
+# if you use negative numbers
+# tit'll start from the end of the string and count back the # of spaces in the 1st number
+# with the last letter in the string being #1
+# and print out until the # of spaces from the end of the string = the second number
+print(myString[-11:-2])
+print('--------------------------------------------------')
+# you can also skip characters
+# if you use [X:Y:Z] format
+# X = starting position
+# Y = extracts all of the characters up to and including this
+# Z = and prints out every Zth character starting with the character at position Z
+print(myString)
+print(myString[0:6:2])
+print(myString[:6:2])
+print('--------------------------------------------------')
+# you can use this to ignore or print out characters at certain intervals.
+# for example, you could print out only the commas in these numbers
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+# leaving out the middle number will continue the pattern to the end of the string
+print("So 'numbers[1::3]' will print out the character at position 1(a comma), and the character\n"
+      "every 3 positions forward from there (in this case also commas) until the end of the string.")
+print(numbers[1::3])
+print('--------------------------------------------------')
+# you can cancatenate string literals without using the '+' operator
+print("You " "can " "cancatenate " "string " "literals " "without " "using " "'+'.")
+
