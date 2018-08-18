c7c4d1c3354c7096c804ae65e2a17ec9c3b10c77
Wayne Burris <waterseeker@Hotmail.com>
2018-08-18 13:50:12 -0500

    used methods to print a slice as a comma separated list inside a cancatenated string

--- StringExercises.py
+++ StringExercises.py
@@ -21,3 +21,33 @@ myString = 'The Spanish Inquisition'
 myIndex = 4
 testReturn = returnCharacterAtIndexXFromString(myString, myIndex)
 print('This is the return from a function using variables. \nThe thing at index ' + str(myIndex) + ' is ' + testReturn + '.')
+
+# you can also do negative indices
+# these'll start from the end of the string
+# so -1 would be the last character in the string.
+
+lastLetterInTheString = myString[-1]
+print('The last character in ' + myString + ' is ' + lastLetterInTheString + '.')
+
+# you can access a slice of a string by passing 2 numbers
+# the first number is the beginning index of the string
+# and the second number is the index of the character immediately _after_
+# the last character you want to include in the slice.
+print('The first 3 characters of ' + myString + ' are ' + myString[0:3] + '.')
+
+# how can I use a for loop on the return of the slice to print
+# the slice as a comma-separated list?
+#
+# get the slice
+mySlice = myString[0:3]
+# make an empty string to push to
+insertedCommasIntoSlice = ""
+# length of the slice
+lengthOfSlice = len(mySlice)
+# for loop
+for i in range(lengthOfSlice):
+    if i != lengthOfSlice - 1:
+        insertedCommasIntoSlice += mySlice[i] + ", "
+    else:
+        insertedCommasIntoSlice += "and " + mySlice[i] + "."
+print('The first 3 characters of ' + myString + ' are ' + insertedCommasIntoSlice)
