8c4a58f25ab78db298a18687412e9acc57e1fb04
Wayne Burris <waterseeker@Hotmail.com>
2018-08-18 13:18:36 -0500

    played around with using a function in Python

--- StringExercises.py
+++ StringExercises.py
@@ -5,4 +5,19 @@ print(parrot)
 # print(yourStringHere[X])
 # index is 0 based, so the thing in the first position is 0, not 1
 x = 3
-print('The character at index ' + str(x) + ' is ' + parrot[x])
+print('The character at index ' + str(x) + ' is ' + parrot[x] + '.')
+# so how could I write a function in python
+# that does/uses the same thing?
+# take a string and an index as parameters
+# and return the character at that index in the string that's passed in?
+def returnCharacterAtIndexXFromString(a, b):
+    return a[b]
+
+testReturn = returnCharacterAtIndexXFromString('testString', 4)
+print(testReturn)
+
+#using variables
+myString = 'The Spanish Inquisition'
+myIndex = 4
+testReturn = returnCharacterAtIndexXFromString(myString, myIndex)
+print('This is the return from a function using variables. \nThe thing at index ' + str(myIndex) + ' is ' + testReturn + '.')
