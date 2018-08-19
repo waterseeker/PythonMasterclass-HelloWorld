f7e7c198794887c3012efb7bb847ef97eaa0ffaf
Wayne Burris <waterseeker@Hotmail.com>
2018-08-19 14:59:10 -0500

    adding examples of string replacement to do the same things as string formatting

--- stringFormatting.py
+++ stringFormatting.py
@@ -29,16 +29,27 @@ November: {1}
 December: {2}""".format(28, 30, 31))
 print("--------------------------------------------------------------------------------")
 
-# an old way of doing something like this is...
-print("I am %d years old" % age)
-print("I am %d %s, %d %s old." % (age, "years", 6, "months"))
-print("--------------------------------------------------------------------------------")
 
 # the ** operator is used to raise to a power
 # so X ** Y = X to the power of Y
 print("The ** operator is used to raise to a power.")
 print("2 to the power of 9 is {0}.".format(2 ** 9))
 print("--------------------------------------------------------------------------------")
+
+
+# an old way of doing something like this is...
+# %d is for an integer
+# %s is for a string
+# ------------------------------------------
+# OLD STRING FORMATTING METHOD FROM PYTHON 2
+# ------------------------------------------
+print("I am %d years old" % age)
+print("I am %d %s, %d %s old." % (age, "years", 6, "months"))
+print("--------------------------------------------------------------------------------")
+
+# ------------------------------------------
+# OLD STRING FORMATTING METHOD FROM PYTHON 2
+# ------------------------------------------
 # if you use a number before the letter in the %X operation
 # the number is the # of spaces that the replacement will use
 # for example, the following assigns 2 spaces for the first set of numbers
@@ -48,6 +59,26 @@ for i in range(1, 12):
     print("No. %2d squared is %4d and cubed is %4d" %(i, i ** 2, i ** 3))
 # if you don't assign the # of spaces, it makes it harder to read in this instance
 print("--------------------------------------------------------------------------------")
+# ------------------------------------------
+# OLD STRING FORMATTING METHOD FROM PYTHON 2
+# ------------------------------------------
 print("Without assigning spaces, the output is harder to read")
 for i in range(1, 12):
     print("No. %d squared is %d and cubed is %d" %(i, i ** 2, i ** 3))
+print("--------------------------------------------------------------------------------")
+# ------------------------------------------
+# OLD STRING FORMATTING METHOD FROM PYTHON 2
+# ------------------------------------------
+# you can specifiy the precision of a number by using the spacing
+print("Pi is approximately %12f" % (22 / 7))
+print("Pi is approximately %12.50f" % (22 / 7))
+print("--------------------------------------------------------------------------------")
+
+# Same stuff using the replacement field method
+# putting ':X' inside the field will assign X spaces to the value
+# like the nuber before the letter in the Python2 way.
+# %2d becomes {0:2} if it's pulling the first value {0} back
+for i in range(1, 12):
+    print("No. {0:2} squared is {1:4} and cubed is {2:4}".format(i, i ** 2, i ** 3))
+print("--------------------------------------------------------------------------------")
+
