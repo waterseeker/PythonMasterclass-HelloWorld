36053c5015128ce5e7537087024550a23df73fd6
Wayne Burris <waterseeker@Hotmail.com>
2018-08-21 19:38:13 -0500

    elif example refactor

--- IfProgramFlow.py
+++ IfProgramFlow.py
@@ -12,23 +12,43 @@
 # else:
 #     print("Sorry, {0}. You must be 18 years old to vote. Please come back in {1} years.".format(name, 18 - age))
 
-print("Please guess a number between 1 and 10: ")
+
+
+# elif example
+# print("Please guess a number between 1 and 10: ")
+# guess = int(input())
+#
+# if guess < 5:
+#     print('Please guess higher.')
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry, you have not guessed correctly.")
+# elif guess > 5:
+#     print("Please guess lower.")
+#     guess = int(input())
+#     if guess == 5:
+#         print("Well done, you guessed it!")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it on your first guess!")
+
+# refactoring the code to remove duplication
+print("Please guess a number between 1 and 10.")
 guess = int(input())
 
-if guess < 5:
-    print('Please guess higher.')
+if guess != 5:
+    if guess < 5:
+        print("Please guess higher.")
+    else:
+        print("Please guess lower.")
+
     guess = int(input())
     if guess == 5:
         print("Well done, you guessed it!")
     else:
         print("Sorry, you have not guessed correctly.")
-elif guess > 5:
-    print("Please guess lower.")
-    guess = int(input())
-    if guess == 5:
-        print("Well done, you guessed it!")
-    else:
-        print("Sorry, you have not guessed correctly")
 else:
-    print("You got it on your first guess!")
-
+    print("You got it in one!")
