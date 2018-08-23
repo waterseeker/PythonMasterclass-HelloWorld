b1bdfac1ec5744e2a9e30acaf70cf88ad4576d74
Wayne Burris <waterseeker@Hotmail.com>
2018-08-23 16:41:41 -0500

    another AND syntax

--- moreAdvancedIfElifAndElse.py
+++ moreAdvancedIfElifAndElse.py
@@ -5,5 +5,10 @@ if age >= 16 and age <= 65:
 
 ageUsingParenthesesToMakeConditionsMoreExplicit = int(input("How old are you, really?"))
 
+# using parentheses around conditions makes them easier to read
 if (ageUsingParenthesesToMakeConditionsMoreExplicit >= 16) and (ageUsingParenthesesToMakeConditionsMoreExplicit == age):
     print("Using parentheses makes conditions easier to read.")
+
+# you can also use the following syntax for AND
+if 16 <= age <= 65:
+    print("You can also use the if X >= Y >= Z syntax.")
